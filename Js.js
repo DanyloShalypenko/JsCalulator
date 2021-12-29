b7b0121819a30eefbb40e06bd7fa8ead05084cdc
DanyloShalypenko <89973870+DanyloShalypenko@users.noreply.github.com>
2021-12-29 14:28:36 +0200

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,11 @@
+send.addEventListener("click",fnc);
+function fnc(){
+    let val_usage = admission.value;
+    let val_investing = consumption.value;
+
+    let val_income = parseInt(val_admission) - parseInt(val_consumption))
+        income.value = val_income
+   
+        result.innerHTML = val_result; 
+
+}
